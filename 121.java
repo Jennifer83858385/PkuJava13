e4c858591ec60a2e03b4db1fd7d15116f9b290e7
LucifaZero <lijianfeng@pku.edu.cn>
2016-01-15 00:04:35 +0800

    Update 121.java

--- 121.java
+++ 121.java
@@ -5,6 +5,7 @@ public class Solution {
     public int maxProfit(int[] prices) {
         int lowPrice=Integer.MAX_VALUE;
         int mostPro=0;
+        //new line
         for(int i:prices){
             if(i<lowPrice)lowPrice=i;
             else if(i-lowPrice>mostPro)mostPro=i-lowPrice;
